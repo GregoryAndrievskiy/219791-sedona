fb3a30c1bc0e8a73d0feae1dee536922712f0923
Grigoriy Andrievskiy <Andirevskiy.g@yandex.ru>
2017-03-15 15:19:05 +0400

    ошибки трэвиса

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
   var navMain = document.querySelector('.main-nav');
+  var navBurger = document.querySelector('.page-header__burger');
+  var navCross = document.querySelector('.page-header__cross');
 
   navCross.classList.add('page-header__button--closed');
   navMain.classList.remove('main-nav--nojs');
