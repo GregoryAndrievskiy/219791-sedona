6cdb8a59b6d138dce6f06fad0d6a1cb32b7e535b
Grigoriy Andrievskiy <Andirevskiy.g@yandex.ru>
2017-03-28 15:47:48 +0400

    автоматизация

---
+++ build/js/popup.js
@@ -0,0 +1,19 @@
+  var popSuccess = document.querySelector(".pop-up__success");
+  var popFailure = document.querySelector(".pop-up__failure");
+  var popButtonSuccess = document.querySelector(".pop-up__close--success");
+  var popButtonFail = document.querySelector(".pop-up__close--fail");
+
+  popSuccess.classList.add("pop-up__success--opened");
+  popFailure.classList.add("pop-up__failure--opened");
+
+  popButtonSuccess.addEventListener("click", function() {
+    if (popSuccess.classList.contains("pop-up__success--opened")) {
+      popSuccess.classList.add("pop-up__success--closed");
+    }
+  });
+
+  popButtonFail.addEventListener("click", function() {
+    if (popFailure.classList.contains("pop-up__failure--opened")) {
+      popFailure.classList.add("pop-up__failure--closed");
+    }
+  });
