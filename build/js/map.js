4feadba6bf6010281a91fc7c7b28fefb311494df
Grigoriy Andrievskiy <Andirevskiy.g@yandex.ru>
2017-03-31 15:57:27 +0400

    исправление по критериям Б5, Б28, Д1, Д13

--- build/js/map.js
+++
@@ -1,5 +0,0 @@
-  var mapGoogle = document.querySelector(".order__map-google");
-  var mapGPS = document.querySelector(".order__gps");
-
-  mapGoogle.classList.remove("order__map-google--nojs");
-  mapGPS.classList.add("order__gps--js");
